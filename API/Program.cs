cff1b2a4cce1d939c0d957c5e4e9dc21c10ee799
Redhwan Adel <redhwan.adel96@gmail.com>
2025-07-03 16:50:21 +0300

    Add project files.

---
+++ API/Program.cs
@@ -0,0 +1,20 @@
+using API.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddDbContext<AppDbContext>(opt =>
+{
+    opt.UseSqlServer(builder.Configuration.GetConnectionString("dbcontext"));
+});
+
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.MapControllers();
+
+app.Run();
