3e106ff9af345e3df2a2b42dce6cd031bad36286
Redhwan Adel <redhwan.adel96@gmail.com>
2025-07-05 14:03:55 +0300

    end of section 5 basic auth clint

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -2,20 +2,32 @@ import { HttpClient } from '@angular/common/http';
 import { Component, inject, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { lastValueFrom } from 'rxjs';
+import { Nav } from "../layout/nav/nav";
+import { AccountService } from '../core/services/account-service';
+import { Home } from "../features/home/home";
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [Nav, Home],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App implements OnInit {
   private http = inject(HttpClient);
+  private acoountService = inject(AccountService)
   protected title = 'FreeLink app';
   protected members = signal<any>([])
 
   async ngOnInit() {
     this.members.set(await this.getMembers())
+    this.serCurrentUser();
+  }
+
+  serCurrentUser() {
+    const userString = localStorage.getItem('user');
+    if (!userString) return;
+    const user = JSON.parse(userString);
+    this.acoountService.currentUser.set(user);
   }
 
   async getMembers() {
