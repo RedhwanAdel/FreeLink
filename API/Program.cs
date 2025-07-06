0584d8e3f8f307629bb22c83bc6fae877fe1d9f0
Redhwan Adel <redhwan.adel96@gmail.com>
2025-07-06 19:22:04 +0300

    end of section 8 member repositroy and seedData

--- API/Program.cs
+++ API/Program.cs
@@ -10,6 +10,9 @@ using System.Text;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddScoped<ITokenService, TokenService>();
+builder.Services.AddScoped<IMemberRepository, MemberRepository>();
+
+
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
     .AddJwtBearer(options =>
     {
@@ -42,4 +45,19 @@ app.UseAuthentication();
 app.UseAuthorization();
 app.MapControllers();
 
+
+
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+try
+{
+    var context = services.GetRequiredService<AppDbContext>();
+    await context.Database.MigrateAsync();
+    await Seed.SeedUsers(context);
+}
+catch (Exception ex)
+{
+    var logger = services.GetRequiredService<ILogger<Program>>();
+    logger.LogError(ex, "An error occured during migration");
+}
 app.Run();
