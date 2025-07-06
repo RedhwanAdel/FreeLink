fca78da90c4191fd3b7e9948905d202a1c774678
Redhwan Adel <redhwan.adel96@gmail.com>
2025-07-06 14:52:26 +0300

    end section 6 routing

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,41 +1,15 @@
-import { HttpClient } from '@angular/common/http';
-import { Component, inject, OnInit, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { lastValueFrom } from 'rxjs';
+import { Component, inject } from '@angular/core';
+import { Router, RouterOutlet } from '@angular/router';
 import { Nav } from "../layout/nav/nav";
-import { AccountService } from '../core/services/account-service';
-import { Home } from "../features/home/home";
 
 @Component({
   selector: 'app-root',
-  imports: [Nav, Home],
+  imports: [Nav, RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App implements OnInit {
-  private http = inject(HttpClient);
-  private acoountService = inject(AccountService)
-  protected title = 'FreeLink app';
-  protected members = signal<any>([])
+export class App {
+  protected router = inject(Router);
 
-  async ngOnInit() {
-    this.members.set(await this.getMembers())
-    this.serCurrentUser();
-  }
 
-  serCurrentUser() {
-    const userString = localStorage.getItem('user');
-    if (!userString) return;
-    const user = JSON.parse(userString);
-    this.acoountService.currentUser.set(user);
-  }
-
-  async getMembers() {
-    try {
-      return lastValueFrom(this.http.get('https://localhost:5001/api/members'));
-    } catch (error) {
-      console.log(error);
-      throw error;
-    }
-  }
 }
